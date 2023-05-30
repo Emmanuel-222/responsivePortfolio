d4d2812f50ce177d8fa94589a7072e4cb0278eeb
emmanuelogbuefi <emmanuelogbuefi43@gmail.com>
2023-05-30 06:46:16 -1100

    This is the Js file

---
+++ side_menu.js
@@ -0,0 +1,9 @@
+function openSideBar(){
+    document.getElementById('side-menu').style.marginRight = '500px'
+    document.getElementById('main').style.marginTop = '380px'
+}
+function closeSideBar(){
+    document.getElementById('side-menu').style.marginRight = '0'
+    document.getElementById('main').style.marginTop = '0'
+
+}
