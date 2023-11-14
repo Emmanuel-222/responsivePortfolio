45a67e5662a35d883c7926c8efda43e392daa5d4
Emmanuel-222 <emmanuelogbuefi43@gmail.com>
2023-11-15 00:45:45 +0100

    This is a modified form of the portfolio.

--- side_menu.js
+++
@@ -1,9 +0,0 @@
-function openSideBar(){
-    document.getElementById('side-menu').style.marginRight = '500px'
-    document.getElementById('main').style.marginTop = '380px'
-}
-function closeSideBar(){
-    document.getElementById('side-menu').style.marginRight = '0'
-    document.getElementById('main').style.marginTop = '0'
-
-}
